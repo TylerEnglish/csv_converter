34d7c6013f0c486b6e86bffb33097b7237a9893e
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-08-26 16:12:34 -0500

    Update main_script.py

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -103,7 +103,7 @@ def data_conversion(main_df, main_columns, new_columns, codes, subjobs, dept, a_
     })
 
     # Fill in more of the values
-    df[new_c['Columns'][3]] = df[new_c['Columns'][3]].apply(modify_value,exclude=list)
+    df[new_c['Columns'][3]] = df[new_c['Columns'][3]].apply(modify_value,exclude=list['Job'])
     df[new_c['Columns'][6]] = df[new_c['Columns'][6]].fillna(m_df[m_df.columns[df.columns.get_loc(main_c['Columns'][5])]])
 
     # Fix columns pt 1
