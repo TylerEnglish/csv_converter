7b65e66bd13b06654093f8cabe5ae1ba23cccff7
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-08-26 16:39:00 -0500

    Update main_script.py

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -183,4 +183,4 @@ def data_conversion(main_df, main_columns, new_columns, codes, subjobs, dept, a_
         df[new_c["Columns"][8]]
     )
 
-    return df[df[new_c['Columns'][0]] is not None]
+    return df[df[new_c['Columns'][0]] != None]
