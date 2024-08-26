485420322c5f634e0cd5a502fc8af01efe7eb0af
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-08-26 16:26:52 -0500

    Update main_script.py

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -93,7 +93,7 @@ def data_conversion(main_df, main_columns, new_columns, codes, subjobs, dept, a_
     })
 
     # Fill in more of the values
-    df[new_c['Columns'][3]] = df[new_c['Columns'][3]].apply(lambda x: f"1-{x}" if x not in ad_list['Job'] else x)
+    df[new_c['Columns'][3]] = df[new_c['Columns'][3]].apply(lambda x: f"1-{x}" if x not in ad_list['Job'] and '1-' not in x else x)
     df[new_c['Columns'][6]] = df[new_c['Columns'][6]].fillna(m_df[m_df.columns[df.columns.get_loc(main_c['Columns'][5])]])
 
     # Fix columns pt 1
